d427a3100f656dd280af1bb328ca5189d84b97f6
dylan wang <zywang_dylan@126.com>
2018-08-08 17:43:30 +0800

    booking controller and login

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,5 +1,54 @@
 class Api::V1::BookingsController < Api::V1::BaseController
+  before_action :set_booking, only: [ :show, :edit, :update, :destroy ]
   def index
     @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:event_id, :user_id)
+  end
+
+
+  def render_error
+    render json: { errors: @booking.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :events
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
