3addbba8d42628e034e17e0227c925c0ebefc34a
dylan wang <zywang_dylan@126.com>
2018-08-10 10:56:08 +0800

    add longitude etc columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_085656) do
+ActiveRecord::Schema.define(version: 2018_08_10_022127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_08_09_085656) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+    t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
