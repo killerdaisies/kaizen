70cca42355f301ceefd80541ba79920dc6ecdb2c
dylan wang <zywang_dylan@126.com>
2018-08-13 14:31:28 +0800

    address show and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
     namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :events
-      resources :bookings
-      resources :users
-      post '/login', to: 'login#login'
+      resources :users do
+        resources :events
+        resources :bookings
+      end
+    post '/login', to: 'login#login'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
